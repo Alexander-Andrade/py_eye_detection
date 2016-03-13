e63f15723a309d64ba9bda7f39a1bfdb22efa41a
Alexander-Andrade <veimar.94@gmail.com>
2016-03-13 16:04:06 +0300

    qwerty

--- py_eye_detection/py_eye_detection.py
+++ py_eye_detection/py_eye_detection.py
@@ -2,13 +2,14 @@ import cv2 as cv
 import numpy as np
 import sys
 
+
 def detect_circles(files_list):
-    for i,file in enumerate(files_list):
+    for f_num, file in enumerate(files_list):
         img = cv.imread(file)
         gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
         filtered_gray = cv.medianBlur(gray,5)
-        #filtered_gray = cv.GaussianBlur(gray,(5,5),0)
-        #filtered_gray = gray
+        # filtered_gray = cv.GaussianBlur(gray,(5,5),0)
+        # filtered_gray = gray
         '''
                circles - output vector of found circles (x,y,r)
                method - CV_HOUGH_GRADIENT
@@ -19,8 +20,8 @@ def detect_circles(files_list):
                minRadius
                maxRadius
         '''
-        circles = cv.HoughCircles(filtered_gray,cv.HOUGH_GRADIENT,1,25,
-                                        param1=200,param2=22,minRadius=10,maxRadius=25)
+        circles = cv.HoughCircles(filtered_gray,cv.HOUGH_GRADIENT, 1, 25,
+                                        param1=200, param2=22, minRadius=12, maxRadius=25)
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0,:]:
@@ -28,11 +29,11 @@ def detect_circles(files_list):
                 cv.circle(img,(i[0],i[1]),i[2],(0,255,0),1)
                 # draw the center of the circle
                 cv.circle(img,(i[0],i[1]),2,(0,0,255),2)
-        cv.imshow('circ' + i,img)
-       
+        cv.imshow('circ' + str(f_num),img)
+        cv.waitKey(1)
         name_ext = file.split('.')
         cv.imwrite(name_ext[0] + '_circ.' + name_ext[1], img)
-    cv.waitKey(1)      
+    cv.waitKey()      
     cv.destroyAllWindows()
 
 
